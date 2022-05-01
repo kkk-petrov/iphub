13604447a1e80a6efba1c068f6854fa96c6619ff
kkk-petrov <kkpetrov.dev@gmail.com>
2022-05-01 14:03:45 +0300

    init: init project with header, main block and swiper

---
+++ js/script.js
@@ -0,0 +1,9 @@
+new Swiper('.services__slider',{
+    navigation: {
+        nextEl: '.slider__btn-next',
+        prevEl: '.slider__btn-prev'
+    },
+    slidesPerView: 4,
+    slidesPerGroup: 1
+
+});
