1bb01f5444cb859ac560d60e9a44ffbd29eed917
kkk-petrov <kkpetrov.dev@gmail.com>
2022-06-06 14:45:06 +0300

    made a burger-menu and shooth scroll

--- js/script.js
+++ js/script.js
@@ -123,6 +123,58 @@ const btnNext = document.querySelector(".quiz__btn-next");
 const btnPrev = document.querySelector(".quiz__btn-prev");
 let userAnswers = {};
 
+// Переменные и константы для бургера
+const menu = document.querySelector(".menu__list");
+const menuIcon = document.querySelector(".menu__icon");
+const menuLinks = document.querySelectorAll(".menu__link[data-goto]")
+
+// Бургер-меню
+menuIcon.addEventListener('click', () => {
+    if(menu.classList.contains("active")){
+        menu.classList.remove('active');
+    }else{
+        menu.classList.add('active');
+    }
+})
+menu.addEventListener('click', (e) => {
+    menu.classList.remove('active');
+})
+
+// Плавный скролл
+if(menuLinks.length > 0){
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener('click', onMenuLinkClick)
+    })
+
+    function onMenuLinkClick(e){
+        const menuLink = e.target;
+        console.log(menuLink.dataset.goto, document.querySelector(menuLink.dataset.goto));
+
+        if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            if(window.screen.width > 1024){
+                const gotoValue = gotoBlock.getBoundingClientRect().top + scrollY; 
+
+                window.scrollTo({
+                    top: gotoValue,
+                    behavior: "smooth"
+                });
+                e.preventDefault();
+            } else{
+                const gotoValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('.header').offsetHeight;
+                
+                window.scrollTo({
+                    top: gotoValue,
+                    behavior: "smooth"
+                });
+                e.preventDefault();
+            }
+
+
+        }
+    }
+}
+
 // Слайдер с услугами
 new Swiper('.services__slider',{
     navigation: {
