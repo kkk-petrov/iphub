c13331c4fb1b1a5ed96fe3e822e9e47391ecf963
kkk-petrov <kkpetrov.dev@gmail.com>
2022-06-09 19:45:23 +0300

    made a faq section

--- js/script.js
+++ js/script.js
@@ -129,6 +129,14 @@ const menu = document.querySelector(".menu__list");
 const menuIcon = document.querySelector(".menu__icon");
 const menuLinks = document.querySelectorAll(".menu__link[data-goto]")
 
+// Переменные и константы для FAQ
+const faq = document.querySelector(".faq");
+const faqList = document.querySelector(".faq__list");
+const faqItem = document.querySelector(".faq__list-item");
+const faqQuestion = document.querySelector(".faq__list-question");
+const faqAnswer = document.querySelector(".faq__list-item__answer");
+const faqBtn = document.querySelector(".faq__list-question__image");
+
 // Бургер-меню
 menuIcon.addEventListener('click', () => {
     if(menu.classList.contains("active")){
@@ -147,6 +155,45 @@ page.addEventListener('click', (e) => {
     menu.classList.remove('active');
     menuIcon.classList.remove('active');
 })
+
+// FAQ
+faqList.addEventListener('click', (e) => {
+    // if(e.target.classList.contains("faq__list-item")){
+    //     for(let i = 0;i<faqList.children.length;i++){
+    //         faqList.children[i].classList.remove('active');
+    //         // faqList.children[i].classList.remove('active');
+    //     }
+    //     e.target.classList.add('active');
+    // } else 
+    
+    if(e.target.parentNode.parentNode.classList.contains("faq__list-item")){
+        for(let i = 0;i<faqList.children.length;i++){
+            faqList.children[i].classList.remove('active');
+        }
+        if(e.target.classList.contains('active')){
+            console.log('active')
+            e.target.parentNode.parentNode.classList.remove('active');
+        } else{
+            console.log('ne active')
+            e.target.parentNode.parentNode.classList.add('active');
+        }
+
+
+
+    } else if(e.target.classList.contains('faq__list-question')){
+        for(let i = 0;i<faqList.children.length;i++){
+            faqList.children[i].classList.remove('active');
+        }
+        if(e.target.classList.contains('active')){
+            console.log('active')
+            e.target.classList.remove('active');
+        } else{
+            console.log('ne active')
+            e.target.parentNode.classList.add('active');
+        }
+    }
+})
+
 
 // Плавный скролл
 if(menuLinks.length > 0){
@@ -284,3 +331,5 @@ answers.addEventListener('click', (e) => {
 })
 
 renderQuestions(0); 
+
+
